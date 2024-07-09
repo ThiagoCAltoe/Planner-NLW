c2bdd25992630a0ced02d9dab4a9cce05f6acef0
Thiago <thiago12_fera@hotmail.com>
2024-07-09 17:13:08 -0300

    feat: Implementing db connection with repository

---
+++ src/models/repositories/trips_repository_test.py
@@ -0,0 +1,43 @@
+import pytest # type: ignore
+import uuid
+from datetime import datetime, timedelta
+
+from src.models.settings.db_connection_handler import db_connection_handler
+
+from .trips_repository import TripsRepository
+
+db_connection_handler.connect()
+trip_id = str(uuid.uuid4())
+
+@pytest.mark.skip(reason="This test is not implemented yet.")
+def test_create_trip():
+    conn = db_connection_handler.get_connection()
+    trips_repository = TripsRepository(conn)
+
+    trips_infos = {
+        "id": trip_id,
+        "destination": "Osasco",
+        "start_date": datetime.strptime("02-01-2024", "%d-%m-%Y"),
+        "end_date": datetime.strptime("02-01-2024", "%d-%m-%Y") + timedelta(days=5),
+        "owner_name": "Thiago Costa",
+        "owner_email": "thiagocosta@ucl.br",
+    }
+
+    trips_repository.create_trip(trips_infos)
+
+
+@pytest.mark.skip(reason="This test is not implemented yet.")
+def test_find_trip_by_id():
+    conn = db_connection_handler.get_connection()
+    trips_repository = TripsRepository(conn)
+
+    trip = trips_repository.find_trip_by_id(trip_id)
+    print(trip)
+
+
+@pytest.mark.skip(reason="This test is not implemented yet.")
+def test_update_trip_status():
+    conn = db_connection_handler.get_connection()
+    trips_repository = TripsRepository(conn)
+
+    trips_repository.update_trip_status(trip_id)
