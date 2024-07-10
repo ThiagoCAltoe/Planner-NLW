5bfc46eed8a204310d4b7703816aff338476d478
Thiago <thiago12_fera@hotmail.com>
2024-07-09 22:30:39 -0300

    feat: Implementing controllers from trips

--- src/main/routes/trips_routes.py
+++ src/main/routes/trips_routes.py
@@ -1,7 +1,59 @@
-from flask import jsonify, Blueprint
+from flask import Blueprint, jsonify, request  # type: ignore
 
-trips_routes_bp = Blueprint('trip_routes', __name__)
+trips_routes_bp = Blueprint("trip_routes", __name__)
 
-@trips_routes_bp.route('/trips', methods=['POST'])
+# Import Controllers
+
+from src.controllers.trip_creator import TripCreator
+from src.controllers.trip_finder import TripFinder
+from src.controllers.trip_confirmer import TripConfirmer
+from src.controllers.link_creator import LinkCreator
+
+# Import Repositories
+
+from src.models.repositories.trips_repository import TripsRepository
+from src.models.repositories.emails_to_invite_repository import EmailsToInviteRepository
+from src.models.repositories.links_repositoriy import LinksRepository
+
+# Import DB Connection Handler
+
+from src.models.settings.db_connection_handler import db_connection_handler
+
+
+@trips_routes_bp.route("/trips", methods=["POST"])
 def create_trip():
-    return jsonify({'message': 'Trip created!'}), 200
+    conn = db_connection_handler.get_connection()
+    trips_repository = TripsRepository(conn)
+    emails_repository = EmailsToInviteRepository(conn)
+    controller = TripCreator(trips_repository, emails_repository)
+
+    response = controller.create(request.json)
+    return jsonify(response["body"]), response["status_code"]
+
+@trips_routes_bp.route("/trips/<tripId>", methods=["GET"])
+def find_trip(tripId):
+    conn = db_connection_handler.get_connection()
+    trips_repository = TripsRepository(conn)
+    controller = TripFinder(trips_repository)
+
+    response = controller.find_trip_details(tripId)
+    return jsonify(response["body"]), response["status_code"]
+
+@trips_routes_bp.route("/trips/<tripId>/confirm", methods=["GET"])
+def confirm_trip(tripId):
+    conn = db_connection_handler.get_connection()
+    trips_repository = TripsRepository(conn)
+    controller = TripConfirmer(trips_repository)
+
+    response = controller.confirm(tripId)
+    return jsonify(response["body"]), response["status_code"]
+
+
+@trips_routes_bp.route("/trips/<tripId>/links", methods=["POST"])
+def create_trip_link(tripId):
+    conn = db_connection_handler.get_connection()
+    links_repository = LinksRepository(conn)
+    controller = LinkCreator(links_repository)
+
+    response = controller.create(request.json, tripId)
+    return jsonify(response["body"]), response["status_code"]
